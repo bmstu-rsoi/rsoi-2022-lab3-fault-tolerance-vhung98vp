57e0086be8d4380fc44a04cafdfaf7e46c053495
Phùng Hùng <vhung98vp@gmail.com>
2022-11-21 22:46:12 +0300

    Fix breaker

--- gateway/src/breakers.js
+++ gateway/src/breakers.js
@@ -4,20 +4,45 @@ const RATING_URL = process.env.RATING_URL || 'http://localhost:8050';
 const LIBRARY_URL = process.env.LIBRARY_URL || 'http://localhost:8060';
 const RESERVATION_URL = process.env.RESERVATION_URL || 'http://localhost:8070';
 
-function unstableRequest(host) {
+function ratingRequest() {
     return new Promise((resolve, reject) => {
         let response;
         try {
-            response = axios.get(host + '/manage/health');
+            response = axios.get(RATING_URL + '/manage/health');
             resolve({message: "Success"})
         } catch(error) {
             reject({message: error})
         }
     })
 }
-const ratingBreaker = new CircuitBreaker(unstableRequest(RATING_URL));
-const libraryBreaker = new CircuitBreaker(unstableRequest(LIBRARY_URL));
-const reservationBreaker = new CircuitBreaker(unstableRequest(RESERVATION_URL));
+
+function libraryRequest() {
+    return new Promise((resolve, reject) => {
+        let response;
+        try {
+            response = axios.get(LIBRARY_URL + '/manage/health');
+            resolve({message: "Success"})
+        } catch(error) {
+            reject({message: error})
+        }
+    })
+}
+
+function reservationRequest() {
+    return new Promise((resolve, reject) => {
+        let response;
+        try {
+            response = axios.get(RESERVATION_URL + '/manage/health');
+            resolve({message: "Success"})
+        } catch(error) {
+            reject({message: error})
+        }
+    })
+}
+
+const ratingBreaker = new CircuitBreaker(ratingRequest);
+const libraryBreaker = new CircuitBreaker(libraryRequest);
+const reservationBreaker = new CircuitBreaker(reservationRequest);
 const BREAKERS = [ratingBreaker, libraryBreaker, reservationBreaker];
 
 module.exports = BREAKERS;
