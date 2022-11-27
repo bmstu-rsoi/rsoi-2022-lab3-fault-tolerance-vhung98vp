1ef89111d8ec382cf78f749cfcf7962ae1c46dd8
Phùng Hùng <vhung98vp@gmail.com>
2022-11-27 16:25:25 +0300

    Update queue time

--- gateway/src/rsmq.js
+++ gateway/src/rsmq.js
@@ -27,7 +27,7 @@ setInterval(() => {
             let {method, url, headers, body} = JSON.parse(resp.message);
             if(method == 'patch'){
                 try {
-                    axios.patch(url, headers, body);
+                    axios.patch(url, headers, body, {timeout: 200});
                     rsmq.deleteMessage({ qname: "APPQUEUE", id: resp.id }, (err) => {
                         if (err) {
                            console.error(err);
